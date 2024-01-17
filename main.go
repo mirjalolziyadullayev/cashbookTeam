d4b059be4ef96c78af7917d9e95beec18c3984c7
Your Name <121923945+mirjalolziyadullayev@users.noreply.github.com>
2024-01-17 11:41:53 +0500

    comments

--- main.go
+++ main.go
@@ -15,4 +15,28 @@ func main() {
 	http.HandleFunc("/transaction", handlers.TransactionHandler)
 
 	http.ListenAndServe(":8080", nil)
+
+	/*
+	Synchronous
+
+		Main thread 
+		(process 1 8s
+		process 2 4s
+		process 3 6s
+		process 4 2s)
+
+		Multithread
+		1 thread process 1
+		2 thread process 2
+		3 thread process 3
+		4 thread process 4
+
+	Asynchronous
+		Main thread {
+			process 1 8s
+			process 2 4s
+			process 3 6s
+			process 4 2s
+		}
+	*/
 }
